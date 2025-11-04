fe821ae0a2820a6fe8b202ac16e0171eb1283675
minhle2312206-a11y <minhle2312206@gmail.com>
2025-11-04 20:57:06 +0700

    Add files via upload

--- ss10/btth.c
+++ ss10/btth.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int a[100];    // mảng tĩnh nhưng không define MAX
-    int n;         // số phần tử hiện có
+    int a[100];
+    int n;
     int chon;
 
     printf("Nhap so phan tu ban dau (toi da 100): ");
@@ -26,69 +26,92 @@ int main() {
         printf("Nhap lua chon: ");
         scanf("%d", &chon);
 
-        if (chon == 1) {
-            int x, pos;
-            printf("Nhap gia tri can chen: ");
-            scanf("%d", &x);
-            printf("Nhap vi tri chen (0..%d): ", n);
-            scanf("%d", &pos);
-
-            if (pos<0 || pos>n || n>=100) printf("Vi tri sai!\n");
-            else {
-                for (int i=n; i>pos; i--) a[i]=a[i-1];
-                a[pos]=x;
-                n++;
-                printf("Da them.\n");
-            }
-        }
-        else if (chon == 2) {
-            int pos;
-            printf("Nhap vi tri can xoa (0..%d): ", n-1);
-            scanf("%d", &pos);
-
-            if (pos<0 || pos>=n) printf("Vi tri sai!\n");
-            else {
-                for (int i=pos; i<n-1; i++) a[i]=a[i+1];
-                n--;
-                printf("Da xoa.\n");
-            }
-        }
-        else if (chon == 3) {
-            int pos, x;
-            printf("Nhap vi tri can cap nhat (0..%d): ", n-1);
-            scanf("%d", &pos);
-            if (pos<0 || pos>=n) printf("Vi tri sai!\n");
-            else {
-                printf("Nhap gia tri moi: ");
+        switch(chon)
+        {
+            case 1: {
+                int x, pos;
+                printf("Nhap gia tri can chen: ");
                 scanf("%d", &x);
-                a[pos]=x;
-                printf("Da cap nhat.\n");
-            }
-        }
-        else if (chon == 4) {
-            int x, found=-1;
-            printf("Nhap gia tri can tim: ");
-            scanf("%d", &x);
-            for (int i=0;i<n;i++)
-                if (a[i]==x){ found=i; break; }
+                printf("Nhap vi tri chen (0..%d): ", n);
+                scanf("%d", &pos);
 
-            if (found==-1) printf("Khong tim thay!\n");
-            else printf("Tim thay tai vi tri %d\n", found);
-        }
-        else if (chon == 5) {
-            printf("Mang hien tai: ");
-            for (int i=0;i<n;i++) printf("%d ", a[i]);
-            printf("\n");
-        }
-        else if (chon == 6) {
-            for (int i=0;i<n-1;i++)
-                for (int j=0;j<n-1-i;j++)
-                    if (a[j] > a[j+1]) {
-                        int t=a[j];
-                        a[j]=a[j+1];
-                        a[j+1]=t;
-                    }
-            printf("Da sap xep.\n");
+                if (pos<0 || pos>n || n>=100) printf("Vi tri sai!\n");
+                else {
+                    for (int i=n; i>pos; i--) a[i]=a[i-1];
+                    a[pos]=x;
+                    n++;
+                    printf("Da them.\n");
+                }
+                break;
+            }
+
+            case 2: {
+                int pos;
+                printf("Nhap vi tri can xoa (0..%d): ", n-1);
+                scanf("%d", &pos);
+
+                if (pos<0 || pos>=n) printf("Vi tri sai!\n");
+                else {
+                    for (int i=pos; i<n-1; i++) a[i]=a[i+1];
+                    n--;
+                    printf("Da xoa.\n");
+                }
+                break;
+            }
+
+            case 3: {
+                int pos, x;
+                printf("Nhap vi tri can cap nhat (0..%d): ", n-1);
+                scanf("%d", &pos);
+                if (pos<0 || pos>=n) printf("Vi tri sai!\n");
+                else {
+                    printf("Nhap gia tri moi: ");
+                    scanf("%d", &x);
+                    a[pos]=x;
+                    printf("Da cap nhat.\n");
+                }
+                break;
+            }
+
+            case 4: {
+                int x, found=-1;
+                printf("Nhap gia tri can tim: ");
+                scanf("%d", &x);
+                for (int i=0;i<n;i++)
+                {
+                    if (a[i]==x){ found=i; break; }
+                }
+                if (found==-1) printf("Khong tim thay!\n");
+                else printf("Tim thay tai vi tri %d\n", found);
+                break;
+            }
+
+            case 5: {
+                printf("Mang hien tai: ");
+                for (int i=0;i<n;i++) printf("%d ", a[i]);
+                printf("\n");
+                break;
+            }
+
+            case 6: {
+                for (int i=0;i<n-1;i++)
+                    for (int j=0;j<n-1-i;j++)
+                        if (a[j] > a[j+1]) {
+                            int t=a[j];
+                            a[j]=a[j+1];
+                            a[j+1]=t;
+                        }
+                printf("Da sap xep tang dan.\n");
+                break;
+            }
+
+            case 7: {
+                printf("Ket thuc chuong trinh!\n");
+                break;
+            }
+
+            default:
+                printf("Lua chon khong hop le!\n");
         }
 
     } while(chon != 7);
